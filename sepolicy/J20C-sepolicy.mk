3dfb7508159898db6e83b62cda6f95a9f6894277
Richard Raya <rdxzv.dev@gmail.com>
2022-11-02 21:21:44 -0300

    surya: Import J20C-sepolicy
    https://github.com/aosp-gr/device_xiaomi_J20C-sepolicy HEAD at 6d6685c4011e4c4c6bffc9626f06c73327248a6b
    
    Change-Id: I89ea6bc8ff4f9b33dfc01c05308b0ad4012fce6f

---
+++ sepolicy/J20C-sepolicy.mk
@@ -0,0 +1,14 @@
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    device/xiaomi/surya/sepolicy/audio/vendor \
+    device/xiaomi/surya/sepolicy/battery/vendor \
+    device/xiaomi/surya/sepolicy/camera/vendor \
+    device/xiaomi/surya/sepolicy/common/vendor \
+    device/xiaomi/surya/sepolicy/fingerprint/vendor \
+    device/xiaomi/surya/sepolicy/ir/vendor \
+    device/xiaomi/surya/sepolicy/leds/vendor \
+    device/xiaomi/surya/sepolicy/power/vendor \
+    device/xiaomi/surya/sepolicy/qcom/vendor \
+    device/xiaomi/surya/sepolicy/thermal/vendor
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/xiaomi/surya/sepolicy/common/private
