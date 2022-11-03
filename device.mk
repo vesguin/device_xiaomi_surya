744c7242d6aa00f8802c577362f86490cc0e3289
Richard Raya <rdxzv.dev@gmail.com>
2022-11-03 15:16:43 -0300

    surya: Add android.frameworks.sensorservice@1.0
    Change-Id: I0d6f8b95fe7483ab5b30a79aa79d1ff2bd2e25fe

--- device.mk
+++ device.mk
@@ -200,6 +200,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
